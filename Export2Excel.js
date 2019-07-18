c936d5b186a7bd3b5ff7170f0bcb60f198718a79
webhjs <49713315+webhjs@users.noreply.github.com>
2019-07-18 16:57:32 +0800

    Update Export2Excel.js

--- Export2Excel.js
+++ Export2Excel.js
@@ -117,7 +117,138 @@ function s2ab(s) {
   return buf;
 }
 
-export function export_table_to_excel_custom(data,name) {
+
+
+export function export_table_title_custom(data,name) {  //id选择器第一行灰色
+  var ws_name = "SheetJS";
+  var wb = new Workbook();
+  wb.SheetNames.push(ws_name);
+  wb.Sheets[ws_name] = data;
+  var dataInfo = wb.Sheets[wb.SheetNames[0]];
+
+  dataInfo['!merges'] && addRangeBorder(dataInfo['!merges'],dataInfo)//定义全局边框
+
+  function addRangeBorder(range,ws){
+    /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+    let arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ","CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"];
+
+    range.forEach(item=>{
+      let startRowNumber = Number(item.s.r),
+        endRowNumber = Number(item.e.r),
+        startColNumber = Number(item.s.c),
+        endColNumber = Number(item.e.c);
+      for(let i = startRowNumber;i<= endRowNumber+1;i++){
+        for(let j = startColNumber;j<= endColNumber;j++){
+          if(!ws[arr[j]+i]) ws[arr[j]+i] = {}
+          ws[arr[j]+i].s = {border:{top:{style:'thin'}, left:{style:'thin'},bottom:{style:'thin'},right:{style:'thin'}}};
+        }
+      }
+    })
+    
+    return ws;
+  }
+
+  
+  const borderAll = {  //单元格外侧框线
+    top: {
+      style: 'thin'
+    },
+    bottom: {
+      style: 'thin'
+    },
+    left: {
+      style: 'thin'
+    },
+    right: {
+      style: 'thin'
+    }
+  };
+  //给所以单元格加上边框
+  for (var i in dataInfo) {
+    if (i == '!ref' || i == '!merges' || i == '!cols' || i == 'A1') {
+
+    } else {
+      dataInfo[i + ''].s = {
+        border: borderAll,
+        alignment: {
+          horizontal: "center",
+          vertical: "center"
+        }
+      }
+    }
+  }
+  
+  // console.log('AA'.charCodeAt(),String.fromCharCode(65))
+  /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+  let map_arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ","CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"];  
+  // let numberRang = data['!ref'].match(/[A-Z]+/g)[1].charCodeAt() - data['!ref'].match(/[A-Z]+/g)[0].charCodeAt() +1
+  let indexRang = map_arr.indexOf(data['!ref'].match(/[A-Z]+/g)[1])
+  
+  for(let i=0;i<=indexRang;i++){
+    //设置副主标题样式
+    dataInfo[map_arr[i]+'1'].s = {
+      font: {
+        // name: '宋体',
+        sz: 14,
+        // color: {rgb: "ff0000"},
+        bold: true,
+        italic: false,
+        underline: false
+      },
+      fill: {
+        fgColor: {rgb: "cccccc"},
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center"
+      },
+    };
+  }
+ 
+
+  dataInfo["!cols"] = [  //单元格列宽
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    },
+    {
+        wpx: 170
+    }
+  ];
+  var wbout = XLSX.write(wb, {
+    bookType: 'xlsx',
+    bookSST: false,
+    type: 'binary'
+  });
+
+  saveAs(new Blob([s2ab(wbout)], {
+    type: "application/octet-stream"
+  }), name)
+}
+
+
+
+export function export_table_to_excel_custom(data,name) {   //id选择器第一行黄色,第二行灰色
   var ws_name = "SheetJS";
   var wb = new Workbook();
   wb.SheetNames.push(ws_name);
@@ -246,7 +377,7 @@ export function export_table_to_excel_custom(data,name) {
   }), name)
 }
 
-export function export_table_to_excel(id) {
+export function export_table_to_excel(id) {     //id选择器无样式表格
   var theTable = document.getElementById(id);
   var oo = generateArray(theTable);
   var ranges = oo[1];
@@ -277,7 +408,7 @@ export function export_table_to_excel(id) {
   }), "test.xlsx")
 }
 
-export function export_json_to_excel(th, jsonData, defaultTitle) {
+export function export_json_to_excel(th, jsonData, defaultTitle) {  //json数据无样式表格
 
   /* original data */
 
@@ -297,7 +428,7 @@ export function export_json_to_excel(th, jsonData, defaultTitle) {
   saveAs(new Blob([s2ab(wbout)], {type: "application/octet-stream"}), title + ".xlsx")
 }
 
-export function export_json_to_excel_custom({
+export function export_json_to_excel_custom({   //json数据设置主题样式
    title,
    multiHeader = [],
    header,
@@ -434,3 +565,338 @@ export function export_json_to_excel_custom({
     type: "application/octet-stream"
   }), `${filename}.${bookType}`);
 }
+
+
+
+
+export function export_json_title_custom({    //json数据设置第一行黄色第二行灰色
+  title,
+  multiHeader = [],
+  header,
+  data,
+  filename,
+  merges = [],
+  autoWidth = true,
+  bookType = 'xlsx'
+ } = {}) {
+ /* original data */
+ filename = filename || 'excel-list'
+ data = [...data]
+ data.unshift(header);
+ data.unshift(title);
+ for (let i = multiHeader.length - 1; i > -1; i--) {
+   data.unshift(multiHeader[i])
+ }
+
+ var ws_name = "SheetJS";
+ var wb = new Workbook(),
+   ws = sheet_from_array_of_arrays(data);
+
+ if (merges.length > 0) {
+   if (!ws['!merges']) ws['!merges'] = [];
+   merges.forEach(item => {
+     ws['!merges'].push(XLSX.utils.decode_range(item))
+   })
+ }
+
+
+ if (autoWidth) {
+   /*设置worksheet每列的最大宽度*/
+   data.shift(title)
+   const colWidth = data.map(row => row.map(val => {
+     /*先判断是否为null/undefined*/
+     if (val == null) {
+       return {
+         'wch': 10
+       };
+     }
+     /*再判断是否为中文*/
+     else if (val.toString().charCodeAt(0) > 255) {
+       return {
+         'wch': val.toString().length * 2
+       };
+     } else {
+       return {
+         'wch': val.toString().length
+       };
+     }
+   }))
+   /*以第一行为初始值*/
+   let result = colWidth[0];
+   for (let i = 1; i < colWidth.length; i++) {
+     for (let j = 0; j < colWidth[i].length; j++) {
+       if (result[j]['wch'] < colWidth[i][j]['wch']) {
+         result[j]['wch'] = colWidth[i][j]['wch'];
+       }
+     }
+   }
+   ws['!cols'] = result;
+ }
+
+ /* add worksheet to workbook */
+ wb.SheetNames.push(ws_name);
+ wb.Sheets[ws_name] = ws;
+ var dataInfo = wb.Sheets[wb.SheetNames[0]];
+
+ const borderAll = {  //单元格外侧框线
+   top: {
+     style: 'thin'
+   },
+   bottom: {
+     style: 'thin'
+   },
+   left: {
+     style: 'thin'
+   },
+   right: {
+     style: 'thin'
+   }
+ };
+ //给所以单元格加上边框
+ for (var i in dataInfo) {
+   if (i == '!ref' || i == '!merges' || i == '!cols' || i == 'A1') {
+
+   } else {
+     dataInfo[i + ''].s = {
+       border: borderAll,
+       alignment: {
+          horizontal: "center",
+          vertical: "center"
+        }
+     }
+   }
+ }
+
+ // 去掉标题边框
+ /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+ let arr = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1", "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "V1", "W1", "X1", "Y1", "Z1"];
+ arr.some(function (v) {
+   let a = merges[0].split(':')
+   if (v == a[1]) {
+     dataInfo[v].s = {}
+     return true;
+   } else {
+     dataInfo[v].s = {}
+   }
+ })
+
+ //设置主标题样式
+ dataInfo["A1"].s = {
+   font: {
+     name: '宋体',
+     sz: 18,
+    //  color: {rgb: "ff0000"},
+     bold: true,
+     italic: false,
+     underline: false
+   },
+   alignment: {
+     horizontal: "center",
+     vertical: "center"
+   },
+   fill: {
+      fgColor: {rgb: "FCFF40"},
+   },
+ };
+
+   // console.log('AA'.charCodeAt(),String.fromCharCode(65))
+  /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+  let map_arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ","CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"];  
+  // let numberRang = data['!ref'].match(/[A-Z]+/g)[1].charCodeAt() - data['!ref'].match(/[A-Z]+/g)[0].charCodeAt() +1
+  let indexRang = map_arr.indexOf(dataInfo['!ref'].match(/[A-Z]+/g)[1])
+  
+  for(let i=0;i<=indexRang;i++){
+    //设置副主标题样式
+    dataInfo[map_arr[i]+'2'].s = {
+      font: {
+        // name: '宋体',
+        sz: 14,
+        // color: {rgb: "ff0000"},
+        bold: true,
+        italic: false,
+        underline: false
+      },
+      fill: {
+        fgColor: {rgb: "cccccc"},
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center"
+      },
+    };
+  }
+  
+  // dataInfo["!cols"] = [  //单元格列宽
+  //   {
+  //       wpx: 70
+  //   }
+  // ];
+
+
+ var wbout = XLSX.write(wb, {
+   bookType: bookType,
+   bookSST: false,
+   type: 'binary'
+ });
+ saveAs(new Blob([s2ab(wbout)], {
+   type: "application/octet-stream"
+ }), `${filename}.${bookType}`);
+}
+
+export function export_json_top_custom({    //json数据设置第一行为灰色
+  title,
+  multiHeader = [],
+  header,
+  data,
+  filename,
+  merges = [],
+  autoWidth = true,
+  bookType = 'xlsx'
+ } = {}) {
+ /* original data */
+ filename = filename || 'excel-list'
+ data = [...data]
+ data.unshift(header);
+ data.unshift(title);
+ for (let i = multiHeader.length - 1; i > -1; i--) {
+   data.unshift(multiHeader[i])
+ }
+
+ var ws_name = "SheetJS";
+ var wb = new Workbook(),
+   ws = sheet_from_array_of_arrays(data);
+
+ if (merges.length > 0) {
+   if (!ws['!merges']) ws['!merges'] = [];
+   merges.forEach(item => {
+     ws['!merges'].push(XLSX.utils.decode_range(item))
+   })
+ }
+
+
+ if (autoWidth) {
+   /*设置worksheet每列的最大宽度*/
+   data.shift(title)
+   const colWidth = data.map(row => row.map(val => {
+     /*先判断是否为null/undefined*/
+     if (val == null) {
+       return {
+         'wch': 10
+       };
+     }
+     /*再判断是否为中文*/
+     else if (val.toString().charCodeAt(0) > 255) {
+       return {
+         'wch': val.toString().length * 2
+       };
+     } else {
+       return {
+         'wch': val.toString().length
+       };
+     }
+   }))
+   /*以第一行为初始值*/
+   let result = colWidth[0];
+   for (let i = 1; i < colWidth.length; i++) {
+     for (let j = 0; j < colWidth[i].length; j++) {
+       if (result[j]['wch'] < colWidth[i][j]['wch']) {
+         result[j]['wch'] = colWidth[i][j]['wch'];
+       }
+     }
+   }
+   ws['!cols'] = result;
+ }
+
+ /* add worksheet to workbook */
+ wb.SheetNames.push(ws_name);
+ wb.Sheets[ws_name] = ws;
+ var dataInfo = wb.Sheets[wb.SheetNames[0]];
+ console.log(dataInfo)
+
+ const borderAll = {  //单元格外侧框线
+   top: {
+     style: 'thin'
+   },
+   bottom: {
+     style: 'thin'
+   },
+   left: {
+     style: 'thin'
+   },
+   right: {
+     style: 'thin'
+   }
+ };
+ //给所以单元格加上边框
+ for (var i in dataInfo) {
+   if (i == '!ref' || i == '!merges' || i == '!cols' || i == 'A1') {
+
+   } else {
+     dataInfo[i + ''].s = {
+       border: borderAll,
+       alignment: {
+          horizontal: "center",
+          vertical: "center"
+        }
+     }
+   }
+ }
+
+ // 去掉标题边框
+ /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+ let arr = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1", "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "V1", "W1", "X1", "Y1", "Z1"];
+ arr.some(function (v) {
+   let a = merges[0].split(':')
+   if (v == a[1]) {
+     dataInfo[v].s = {}
+     return true;
+   } else {
+     dataInfo[v].s = {}
+   }
+ })
+
+
+   // console.log('AA'.charCodeAt(),String.fromCharCode(65))
+  /* 写死的数组不能灵活适应26进制表格数据,对于本项目问题不大 */
+  let map_arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ","CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"];  
+  // let numberRang = data['!ref'].match(/[A-Z]+/g)[1].charCodeAt() - data['!ref'].match(/[A-Z]+/g)[0].charCodeAt() +1
+  let indexRang = map_arr.indexOf(dataInfo['!ref'].match(/[A-Z]+/g)[1])
+  
+  for(let i=0;i<=indexRang;i++){
+    //设置副主标题样式
+    dataInfo[map_arr[i]+'1'].s = {
+      font: {
+        // name: '宋体',
+        sz: 14,
+        // color: {rgb: "ff0000"},
+        bold: true,
+        italic: false,
+        underline: false
+      },
+      fill: {
+        fgColor: {rgb: "cccccc"},
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center"
+      },
+    };
+  }
+  
+  // dataInfo["!cols"] = [  //单元格列宽
+  //   {
+  //       wpx: 70
+  //   }
+  // ];
+
+
+ var wbout = XLSX.write(wb, {
+   bookType: bookType,
+   bookSST: false,
+   type: 'binary'
+ });
+ saveAs(new Blob([s2ab(wbout)], {
+   type: "application/octet-stream"
+ }), `${filename}.${bookType}`);
+}
+
